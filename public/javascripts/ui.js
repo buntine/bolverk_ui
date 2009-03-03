ba363ed2f91b7a26f6b81a1e839cc11d58ae39ad
Andy Buntine <info@andrewbuntine.com>
2009-03-03 23:47:31 +1100

    Started working on cell edit functionality

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -2,12 +2,20 @@ $(document).ready(function(){
 
   // Control the selected cell.
   $("#main_memory table.cells div").click(function(){
-    add_class = !$(this).hasClass("selected_cell");
     $("#main_memory table.cells div").each(function(){
       $(this).removeClass("selected_cell");
     });
-    if (add_class)
-      $(this).addClass("selected_cell");
+    $(this).addClass("selected_cell");
+  });
+
+  // Render the cell editable on a double-click.
+  $("#main_memory table.cells div").dblclick(function(){
+    hex = $(this).text();
+    text_box = document.createElement("input");
+    text_box.setAttribute("type", "text");
+    text_box.setAttribute("value", hex);
+    text_box.style.width = '18px';
+    $(this).html(text_box);
   });
 
 });
