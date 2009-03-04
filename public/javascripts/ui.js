d9593406c5f72147a1adf54e7fde065e2c79879c
buntine <andrew.buntine@ansearch.com>
2009-03-04 17:08:18 +1100

    Kept working on UI

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -10,12 +10,21 @@ $(document).ready(function(){
 
   // Render the cell editable on a double-click.
   $("#main_memory table.cells div").dblclick(function(){
-    hex = $(this).text();
-    text_box = document.createElement("input");
-    text_box.setAttribute("type", "text");
-    text_box.setAttribute("value", hex);
-    text_box.style.width = '18px';
-    $(this).html(text_box);
+    if (!is_being_edited(this)) {
+      hex = $(this).html();
+      text_box = document.createElement("input");
+      text_box.setAttribute("type", "text");
+      text_box.setAttribute("value", hex);
+      text_box.setAttribute("maxlength", "2");
+      text_box.style.width = '18px';
+      $(this).html(text_box);
+    }
   });
+
+  // Helper method.
+  // Returns true if the passed-in cell contains anything other than a 2-byte string.
+  function is_being_edited(cell) {
+    return $(cell).html().length > 2;
+  }
 
 });
