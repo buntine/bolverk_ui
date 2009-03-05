7c5096f24b66d8ddeef5e04de9729e2987b9b55b
buntine <andrew.buntine@ansearch.com>
2009-03-05 17:26:31 +1100

    Refined some of the JS functionality

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -19,7 +19,14 @@ $(document).ready(function(){
       text_box.setAttribute("maxlength", "2");
       text_box.className = "cellcontents";
       text_box.style.width = '18px';
+
+      // Store the hidden val so we can reference it later.
+      current_val = document.createElement("input");
+      current_val.setAttribute("type", "hidden");
+      current_val.setAttribute("value", hex);
+
       $(this).html(text_box);
+      $(this).append(current_val);
       $(text_box).focus();
     }
   });
@@ -27,16 +34,25 @@ $(document).ready(function(){
   // Save the updated contents of a memory cell after
   // it has been modified.
   $('.cellcontents').livequery('blur', function(){
+    original_value = $(this).next().val();
     value = $(this).val().rjust(2, "0");
-    if (!is_valid_base_16(value)) {
-      value = '00';
-    }
-    $(this).parent().html(value.toUpperCase());
+    cell = $(this).parent();
+    if (!is_valid_base_16(value)) { value = '00'; }
+
+    if (value != original_value) {
+      cell.html(value.toUpperCase());
+      //$(this).parent().html('<img src="images/ajax-small.gif" />');
+    } else
+      cell.html(value.toUpperCase());
+
+    // Configure the display for the cell based on the value we just stored in there.
+    if (value != "00") { cell.removeClass("selected_cell"); cell.addClass("populated_cell"); }
+    else { cell.removeClass("populated_cell"); cell.addClass("selected_cell"); }
   });
 
-  // Returns true if 'cell' contains anything other than a 2-byte string.
+  // Returns true if 'cell' contains anything other base-16 string.
   function is_being_edited(cell) {
-    return $(cell).html().length > 2;
+    return !is_valid_base_16($(cell).html());
   }
   
   // Returns true if 'value' is valid hexadecimal (length being 2).
