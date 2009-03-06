2188e52713a8c4e9aac94f2b5237486d7b2f88a6
buntine <andrew.buntine@ansearch.com>
2009-03-06 14:56:26 +1100

    Added cell indicators to registers

--- helpers/ui.rb
+++ helpers/ui.rb
@@ -4,37 +4,37 @@ helpers do
   <<-EOF
     <table align="center" cellpadding="0" cellspacing="0" class="cells">
       <tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">0</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">1</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">2</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">3</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">4</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">5</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">6</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">7</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">8</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">9</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">A</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">B</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">C</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">D</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">E</span></div></td>
       </tr><tr>
-        <td><div>00</div></td>
+        <td><div><span>00</span><span class="hex">F</span></div></td>
       </tr>
     </table>
   EOF
