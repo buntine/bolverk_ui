ea4dd45a03125c7d8b5400c2c8ba7eac40d73603
buntine <andrew.buntine@ansearch.com>
2009-03-12 09:58:20 +1100

    Refactored a few bits of logic. After hooks would be cooler

--- bolverk_ui.rb
+++ bolverk_ui.rb
@@ -24,6 +24,11 @@ helpers do
     session_file.write(Marshal.dump(emulator))
     session_file.close
   end
+
+  def save_and_render
+    write_emulator(@emulator)
+    erb :index
+  end
 end
 
 before do
@@ -89,21 +94,21 @@ post '/program' do
   cell = params[:cell]
   instructions = (params[:instructions] || "").split(" ")
   @emulator.load_program_into_memory(cell, instructions)
-  write_emulator(@emulator)
-  erb :index
+  save_and_render
 end
 
 # Clear main memory, registers and flush stdout.
 post '/reboot' do
-  # MIGHT NEED TO ADD THIS FUNCTIONALITY TO ENGINE...
+  @emulator.reboot!
+  session[:stdout] = ""
+  save_and_render
 end
 
 # Initialise a program in the processor.
 post %r{/program/start/([a-fA-F0-9]{2})} do
   cell = params[:captures][0]
   @emulator.start_program(cell)
-  write_emulator(@emulator)
-  erb :index
+  save_and_render
 end
 
 # Perform a machine cycle and render the effects.
@@ -111,8 +116,7 @@ post '/program/step' do
   session[:stdout] << OutputCatcher.catch_out do
     @emulator.perform_machine_cycle
   end
-  write_emulator(@emulator)
-  erb :index
+  save_and_render
 end
 
 # Write to a memory cell.
@@ -120,6 +124,5 @@ post %r{/write/([a-fA-F0-9]{2})/([a-fA-F0-9]{2})} do
   cell = params[:captures][0]
   value = params[:captures][1]
   @emulator.memory_write(cell, value.clone)
-  write_emulator(@emulator)
-  body(value)
+  save_and_render
 end
